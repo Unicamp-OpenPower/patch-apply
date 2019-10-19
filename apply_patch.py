4076ce762af1fe4d399435e198ca210c9c8467ba
Yukine28 <c195727@dac.unicamp.br>
2019-10-19 00:14:49 -0300

    Update apply_patch.py

--- apply_patch.py
+++ apply_patch.py
@@ -10,14 +10,15 @@ def _args():
 
     return argv[1], argv[2]
 
-def insert_patch(str_orig, str_search, str_insert, mode="top"):
+def insert_patch(str_orig, str_search, str_insert, mode):
 
-    if mode == "top":
-        position = str_orig.index(str_search)
+    if mode == "final":
+        str_orig.append(str_insert)
     else:
-        position = str_orig.index(str_search) + str_search.len()
+        position = (str_orig).index(str_search) + 1
+        str_orig.insert(position, str_insert)
 
-    return str_orig.insert(position, str_insert)
+    return str_orig
 
 def main():
     base_dir, patchs_dir = _args()
@@ -34,29 +35,35 @@ def main():
         patch_file = open(f, "r")
         patch_list = patch_file.readlines()
         patch_file.close()
-
+        
         # First line of patch: original file location
-        orig_file = open(base_dir + "/" + patch_list[0], "r")
+        path_to_file = patch_list[0].split() #Remove '\n' in end of the line
+        orig_file = open(base_dir + "/" + path_to_file[0], "r")
         orig_list = orig_file.readlines()
         orig_file.close()
 
         # Second line of patch: options
-        options = patch_list[1].split("")
+        options = patch_list[1].split()
 
         # Patch number of lines and mode (top or bottom), separated by a space
         patch_size = int(options[0])
         patch_mode = options[1]
 
         # Get patch strings
-        str_insert = patch_list[2:(2+patch_size)]
-        str_search = patch_list[(3+patch_size):]
+        list_insert = patch_list[2:(2+patch_size)]
+        str_insert = ''.join(list_insert)
+        list_search = patch_list[(3+patch_size):]
+        str_search = ''.join(list_search)
 
         # Apply patch
-        output = insert_patch(orig_list, str_search, str_insert, mode=patch_mode)
+        output = insert_patch(orig_list, str_search, str_insert, patch_mode)
 
         # Write patched file
-        orig_file = open(base_dir + "/" + patch_list[0], "w")
+        path_to_file = patch_list[0].split() #Remove '\n' in end of the line
+        orig_file = open(base_dir + "/" + path_to_file[0], "w")
         orig_file.writelines(output)
         orig_file.close()
 
         print("File " + base_dir + "/" + patch_list[0] + " modified")
+
+main()
